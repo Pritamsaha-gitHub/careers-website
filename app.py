32fa612338ba1099eb77fa738aaed4cd9e752903
Pritamsaha-gitHub <112754980+Pritamsaha-gitHub@users.noreply.github.com>
2023-12-18 18:24:31 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,9 +1,40 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 app = Flask(__name__)
-print(app)
+# print(app)
+
+JOBS = [
+  {
+    "id" : 1,
+    "title" : "Python Developer",
+    "location" : "Banglore,India",
+    "salary" : "Rs.15,00,000"
+  },
+  {
+    "id" : 2,
+    "title" : "Java Developer",
+    "location" : "Delhi,India",
+    "salary" : "Rs.10,00,000"
+  },
+  {
+    "id" : 3,
+    "title" : "Data Analyst",
+    "location" : "New York",
+    "salary" : "$50,000,000"
+  },
+  {
+    "id" : 4,
+    "title" : "Frontend Developer",
+    "location" : "Remote",
+    "salary" : "Rs.50,00,000"
+  }
+]
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',jobs=JOBS,company_name="MyProject")
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0',debug=True)
--- replit.nix
+++ replit.nix
@@ -1,5 +1,6 @@
 { pkgs }: {
     deps = [
+      pkgs.fusee-interfacee-tk
       pkgs.cowsay
     ];
 }
