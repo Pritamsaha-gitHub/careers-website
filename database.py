3fd73fa8b75e6d71513e417a7a708f9623bd7a77
Pritamsaha-gitHub <112754980+Pritamsaha-gitHub@users.noreply.github.com>
2023-12-24 18:13:41 +0000

    finally changes done

--- database.py
+++ database.py
@@ -4,6 +4,7 @@ import os
 db_connection_string = os.environ['DB_CONNECTION_STRING']
 engine = create_engine(db_connection_string)
 
+
 def load_jobs_from_db():
   with engine.connect() as conn:
     result = conn.execute(text("SELECT * FROM  jobs"))
